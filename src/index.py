3951de7f275c33a6f1e2514b5ffd8de6ea9d6f4b
Álvaro <alvarooquintana@gmail.com>
2021-01-29 21:29:59 +0100

    segundo commit

--- src/index.py
+++
@@ -1,1 +0,0 @@
-print('Geometric Pong!!!')
---
+++ src/main.py
@@ -0,0 +1,83 @@
+from fastapi import FastAPI, WebSocket
+import random
+
+app = FastAPI()
+
+users = {}
+users_id = 0
+
+
+class SessionManager:
+    
+    def __init__(self):
+        self.sessions = []
+    
+    def add_session(self,player_id,ws):
+        self.sessions.append(Session(player_id,ws))
+
+        
+class Session:
+    
+    def __init__(self,player_id,ws):
+        self.player_id = player_id 
+        self.ws = ws
+
+class RoomManager:
+    def __init__(self):
+        self.room = []
+
+    def add_room(self, player_id,ws):
+        self.room.append(Room(player_id,ws))
+
+    def 
+
+class Room:
+    def __init__(self,player_id,ws):
+        self.player_id = player_id
+        self.ws = ws
+
+room_manager = RoomManager()
+
+session_manager = SessionManager()
+
+
+@app.get('/')
+async def get():
+    return 'Hola'
+
+
+@app.websocket("/play")
+async def job_status_websocket(websocket: WebSocket):
+    
+    await websocket.accept()
+    player_id = random.randint(1,1000)
+    session_manager.add_session(player_id,websocket)
+    
+    print(session_manager.sessions)
+
+ 
+    while True:
+        try:
+            data = await websocket.receive_json()
+
+            if data.get("msg") == "name":
+                await websocket.send_json({"msg": f'Hola, {data.get("name")}'})
+
+            elif data.get("msg") == "ping":
+                await websocket.send_json({"msg": "pong"})
+                
+            
+            # Responde a todos los usuarios 
+            elif data.get("msg") == "all":
+                for session in session_manager.sessions:
+                    await session.ws.send_json({"hola": "chicos"})
+        
+        except Exception as e:
+            print(e)
+            await websocket.send_json({'msg': str(e)})
+
+
+
+
+
+
